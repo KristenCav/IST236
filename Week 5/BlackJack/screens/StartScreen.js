19434875c099fecd4eb8e14fe12b6d7626595f61
Kristen Cavanagh <kmcavanagh1990@gmail.com>
2024-02-28 17:02:26 -0500

    BlackJack App files from Week 5 and 6

---
+++ Week 5/BlackJack/screens/StartScreen.js
@@ -0,0 +1,66 @@
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { View, StyleSheet, Image } from "react-native";
+import NavButton from "../components/NavButton";
+import Title from "../components/Title";
+
+function StartGameScreen(props) {
+  //Set Safe Area Screen Boundaries - determines size of screen to
+  //ensure we are always within boundaries
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.rootContainer,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}
+    >
+      <View style={styles.titleContainer}>
+        <Title>BlackJack 21</Title>
+      </View>
+
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/blackjackbg.png")}
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <NavButton onPress={props.onNext}>Play Now</NavButton>
+      </View>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  imageContainer: {
+    flex: 5,
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    resizeMode: "contain",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
